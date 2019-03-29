41bd5e90413e61c580ca113053c1d386e9b8d746
Miguel Duran <mduran0314@gmail.com>
2019-03-29 00:27:02 -0700

    turn into web service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace Hello
 {
@@ -6,7 +9,17 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseConfiguration(config)
+                .Build();
+
+            host.Run();
         }
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+public class Startup
+{
+    public Startup(IHostingEnvironment env)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app,
+        IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync("Hello World!");
+        });
+    }
+}
